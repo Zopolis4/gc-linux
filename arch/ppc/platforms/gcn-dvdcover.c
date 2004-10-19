c96d9a9c227c85b1f4afdf2bf59d9802513a4744
Albert Herranz <herraa1@users.sourceforge.net>
2004-10-19 23:36:53 +0000

    - Transformed console.{c,h} into gcn-con.{c,h}   Renamed consistenly parts of the code, and rearranged it a little bit.   Remember that this is not a fully functional console but a console suitable   for early debugging as it is available as soon as the setup arch code is run. - Moved gc-dvdcover.c to gcn-dvdcover.c.   Renamed consistenly parts of the code. - Moved gc-rsw.c to gcn-rsw.c.   Renamed consistenly parts of the code. - Added copyright headers to files.

---
+++ arch/ppc/platforms/gcn-dvdcover.c
@@ -0,0 +1,121 @@
+/*
+ * arch/ppc/platforms/gcn-dvdcover.c
+ *
+ * Nintendo GameCube DVD cover driver
+ * Copyright (C) 2004 Stefan Esser
+ * Copyright (C) 2004 The GameCube Linux Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+
+#include <asm/io.h>
+
+#define DVD_IRQ		     2
+
+#define DI_DISR              0xcc006000	/* DI Status Register */
+#define  DI_DISR_BRKINT      (1<<6)
+#define  DI_DISR_BRKINTMASK  (1<<5)
+#define  DI_DISR_TCINT       (1<<4)
+#define  DI_DISR_TCINTMASK   (1<<3)
+#define  DI_DISR_DEINT       (1<<2)
+#define  DI_DISR_DEINTMASK   (1<<1)
+#define  DI_DISR_BRK         (1<<0)
+
+#define DI_DICVR             0xcc006004	/* DI Cover Register */
+#define  DI_DICVR_CVRINT     (1<<2)
+#define  DI_DICVR_CVRINTMASK (1<<1)
+#define  DI_DICVR_CVR        (1<<0)
+
+#define DI_DICMDBUF0         0xcc006008	/* DI Command Buffer 0 */
+
+#define DI_DICR              0xcc00601c	/* DI Control Register */
+#define  DI_DICR_RW          (1<<2)
+#define  DI_DICR_DMA         (1<<1)
+#define  DI_DICR_TSTART      (1<<0)
+
+#define DI_CMD_STOP          (0xE3)
+
+
+#define DRV_MODULE_NAME      "gcn-dvdcover"
+#define DRV_DESCRIPTION      "Nintendo GameCube DVD cover driver"
+#define DRV_AUTHOR           "Stefan Esser <se@nopiracy.de>"
+
+MODULE_DESCRIPTION(DRV_DESCRIPTION);
+MODULE_AUTHOR(DRV_AUTHOR);
+MODULE_LICENSE(GPL);
+
+#define PFX DRV_MODULE_NAME ": "
+#define di_printk(level, format, arg...) \
+	printk(level PFX format , ## arg)
+
+
+/**
+ *
+ */
+static irqreturn_t gcn_dvdcover_handler(int this_irq, void *dev_id,
+					struct pt_regs *regs)
+{
+	unsigned long reason = readl(DI_DICVR);
+
+	/* handle only DVD cover interrupts here */
+	if (reason & DI_DICVR_CVRINT) {
+		writel(reason | DI_DICVR_CVRINT, DI_DICVR);
+		di_printk(KERN_INFO, "DVD cover was %s.\n",
+			  (reason & DI_DICVR_CVR) ? "opened" : "closed");
+
+		return IRQ_HANDLED;
+	}
+	return IRQ_NONE;
+}
+
+/**
+ *
+ */
+static int gcn_dvdcover_init(void)
+{
+	unsigned long outval;
+	int err;
+
+	/* clear pending DI interrupts and mask new ones */
+	/* this prevents an annoying bug while we lack a complete DVD driver */
+	outval = DI_DISR_BRKINT | DI_DISR_TCINT | DI_DISR_DEINT;
+	outval &= ~(DI_DISR_BRKINTMASK | DI_DISR_TCINTMASK | DI_DISR_DEINTMASK);
+	writel(outval, DI_DISR);
+
+	/* stop DVD motor */
+	writel(DI_CMD_STOP << 24, DI_DICMDBUF0);
+	writel(DI_DICR_TSTART, DI_DICR);
+
+	err = request_irq(DVD_IRQ, gcn_dvdcover_handler, 0,
+			  "Nintendo GameCube DVD", 0);
+	if (err) {
+		di_printk(KERN_ERR, "request of irq%d failed\n", DVD_IRQ);
+		return err;
+	}
+
+	/* enable DVD cover interrupts */
+	writel(readl(DI_DICVR) | DI_DICVR_CVRINTMASK, DI_DICVR);
+
+	return 0;
+}
+
+/**
+ *
+ */
+static void gcn_dvdcover_exit(void)
+{
+	free_irq(DVD_IRQ, 0);
+}
+
+module_init(gcn_dvdcover_init);
+module_exit(gcn_dvdcover_exit);
+
